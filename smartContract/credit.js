d817c2019be88e0cf1ea3f22b753519130cadd41
yuanbo <yuanbo@apache.org>
2018-05-13 20:30:24 +0800

    init project

---
+++ smartContract/credit.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var DictItem = function(text) {
+	if (text) {
+		var obj = JSON.parse(text);
+		this.key = obj.key;
+		this.value = obj.value;
+		this.author = obj.author;
+	} else {
+	    this.key = "";
+	    this.author = "";
+	    this.value = 0;
+	}
+};
+
+DictItem.prototype = {
+	toString: function () {
+		return JSON.stringify(this);
+	}
+};
+
+var Credit = function() {
+    LocalContractStorage.defineMapProperty(this, "repo", {
+        parse: function (text) {
+            return new DictItem(text);
+        },
+        stringify: function (o) {
+            return o.toString();
+        }
+    });
+}
+
+
+Credit.prototype = {
+    init: function () {
+        // todo
+    },
+
+    // allow user to update the credit score.
+    update: function() {
+        // todo
+    },
+
+    save: function (key, value) {
+
+        key = key.trim();
+        value = value.trim();
+        if (key === "" || value === ""){
+            throw new Error("empty key / value");
+        }
+        if (value.length > 64 || key.length > 64){
+            throw new Error("key / value exceed limit length")
+        }
+
+        var from = Blockchain.transaction.from;
+        var dictItem = this.repo.get(key);
+        if (dictItem){
+            throw new Error("value has been occupied");
+        }
+
+        dictItem = new DictItem();
+        dictItem.author = from;
+        dictItem.key = key;
+        dictItem.value = value;
+
+        this.repo.put(key, dictItem);
+    },
+
+    get: function (key) {
+        key = key.trim();
+        if ( key === "" ) {
+            throw new Error("empty key")
+        }
+        return this.repo.get(key);
+    }
+};
+
+module.exports = Credit;
